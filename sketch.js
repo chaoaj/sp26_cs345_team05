3f73922f2222563e9a4fc645894a976ce5f085fc
bandnr <bandnr@dukes.jmu.edu>
2026-03-26 15:43:42 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,13 @@ function setup() {
   createCanvas(400, 400);
 }
 
+function homePage(isTrue) {
+  // what is on the home page change isTrue to false when you leave the home page
+
+
+}
+
 function draw() {
   background(220);
+  homePage(true);
 }
