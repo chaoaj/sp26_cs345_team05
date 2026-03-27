0f082033ec6f9bfa877491f59cf74aa055fc43a2
bandnr <bandnr@dukes.jmu.edu>
2026-03-27 09:23:27 -0400

    Update sketch.js

--- dev/sketch.js
+++ dev/sketch.js
@@ -1,7 +1,44 @@
+var page = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
+//page variable tells you what page you are on
+// 0 = home page
+// 1 = skin screen
+// 2 = story slides
+// 3 = gameover screen
+function screen() {
+  if (page == 0) {
+    homePage();
+  } else if (page == 1) {
+    skinScreen();
+  } else if (page == 2) {
+    storySlides();
+  } else if (page == 3) {
+    gameover();
+  }
+}
+function homePage() {
+  
+
+}
+
+function skinScreen() {
+  
+}
+
+function storySlides() {
+  
+}
+
+function gameover() {
+
+}
+  
+
 function draw() {
   background(220);
+  screen(page);
 }
