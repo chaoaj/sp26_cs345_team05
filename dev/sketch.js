328f8d83f582b13acbfd2af9f4b7df28727da97b
bandnr <bandnr@dukes.jmu.edu>
2026-03-27 09:50:49 -0400

    images

--- dev/sketch.js
+++ dev/sketch.js
@@ -3,12 +3,23 @@ var page = 0;
 function setup() {
   createCanvas(400, 400);
 }
+function preload(){
+  game_over = loadImage("assets/game_over.png");
+  skins = loadImage("assets/skins.png");
+  start_game = loadImage("assets/start_game.png");
+  title = loadImage("assets/title.png");
+
+}
 
 //page variable tells you what page you are on
 // 0 = home page
 // 1 = skin screen
 // 2 = story slides
 // 3 = gameover screen
+
+function button() {
+
+}
 function screen() {
   if (page == 0) {
     homePage();
--- sketch.js
+++ sketch.js
@@ -4,11 +4,30 @@ function setup() {
   createCanvas(400, 400);
 }
 
+function preload(){
+  game_over = loadImage("assets/game_over.png");
+  skins = loadImage("assets/skins.png");
+  start_game = loadImage("assets/start_game.png");
+  title = loadImage("assets/title.png");
+
+}
+function button(image, x, y, w, h) {
+  image(image, x, y, w, h);
+  if (mouseClicked() && mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+    if (image == start_game) {
+      page = 2;
+    } else if (image == skins) {
+      page = 1;
+    }
+  }
+}
+
 //page variable tells you what page you are on
 // 0 = home page
 // 1 = skin screen
 // 2 = story slides
 // 3 = gameover screen
+// 4 = victory screen
 function screen() {
   if (page == 0) {
     homePage();
@@ -18,6 +37,8 @@ function screen() {
     storySlides();
   } else if (page == 3) {
     gameover();
+  } else if (page == 4) {
+    victory();
   }
 }
 function homePage() {
@@ -35,6 +56,10 @@ function storySlides() {
 
 function gameover() {
 
+}
+
+function victory() {
+  
 }
   
 
