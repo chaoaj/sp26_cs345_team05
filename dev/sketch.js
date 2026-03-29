b66c70ea16ce2548e2fe30ba53bfe8df14eb0adc
Toshura Peshlakai Washington Jr <washi3tp@dukes.jmu.edu>
2026-03-29 17:22:33 -0400

    reverted all changes

--- dev/sketch.js
+++ dev/sketch.js
@@ -1,29 +1,69 @@
-let imgs = []; // array to hold the images
-let currentImg = 0;
-let num_imgs; // variable to hold the number of images
+var page = 1;
+var scale = 1;
 
-var page = 0;
+let pageWidth = 600;
+let pageHeight = 400;
+
+let homepageX = 0;
+let homepageY = 0;
+
+function preload() {
+  homepage_background = loadImage("assets/homepage_background.png");
+  cat = loadImage("assets/cat_homepage.png");
+  title1 = loadImage("assets/title1.png");
+  title2 = loadImage("assets/title2.png");
+
+  start_game1 = loadImage("assets/start_game1.png");
+  start_game2 = loadImage("assets/start_game2.png");
+
+  skins1 = loadImage("assets/skins1.png");
+  skins2 = loadImage("assets/skins2.png");
+
+  return1 = loadImage("assets/return1.png");
+  return2 = loadImage("assets/return2.png");
+
+  game_over = loadImage("assets/game_over.png");
+  restart = loadImage("assets/restart.png");
+
+
+}
 
 function setup() {
-  createCanvas(400, 400);
-  setInterval(nextImage, 3000); // change image every 3 seconds
+  createCanvas(pageWidth, pageHeight);
 }
-function preload(){
-  game_over = loadImage("assets/game_over.png");
-  skins = loadImage("assets/skins.png");
-  start_game = loadImage("assets/start_game.png");
-  title = loadImage("assets/title.png");
+
+function button(image1, x, y, w, h) {
+  image(image1, x, y, w, h);
+  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+    if (image1 == start_game2) {
+      image(start_game1, 275, 150, start_game1.width/7 * scale, start_game1.height/6 * scale);
+    } else if (image1 == skins2) {
+      image(skins1, 410, 250, skins1.width/7 * scale, skins1.height/6 * scale);
+    } else if (image1 == return2) {
+      image(return1, 20, 20, return1.width/7 * scale, return1.height/6 * scale);
+    }
+  }
+  if (mouseIsPressed && mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+    if (image1 == start_game2) {
+      page = 2;
+    } else if (image1 == skins2) {
+      page = 1;
+    } else if (image1 == return2) {
+      page = 0;
+    } else if (image1 == restart) {
+      page = 0;
+    }
+
+    print(page);
+  }
 }
 
 //page variable tells you what page you are on
 // 0 = home page
 // 1 = skin screen
 // 2 = story slides
-// 3 = gameover screen
-
-function button() {
-
-}
+// 3 = game over screen
+// 4 = victory screen
 function screen() {
   if (page == 0) {
     homePage();
@@ -33,25 +73,121 @@ function screen() {
     storySlides();
   } else if (page == 3) {
     gameover();
+  } else if (page == 4) {
+    victoryPage();
   }
 }
+
 function homePage() {
-  
+  scale = 1;
+  backgroundMoveSpeed = 0.5;
+
+  if (mouseX > pageWidth/2 && homepageX < 0) {
+    homepageX += backgroundMoveSpeed;
+  } else  if (mouseX < pageWidth/2 && mouseX != 0 && homepageX > -pageWidth / 50) {
+    homepageX -= backgroundMoveSpeed;
+  }
+
+  if (mouseY > pageHeight/2 && homepageY < 0) {
+    homepageY += backgroundMoveSpeed;
+  } else  if (mouseY < pageHeight/2 && mouseY != 0 && homepageY > -pageHeight / 50) {
+    homepageY -= backgroundMoveSpeed;
+  }
+
+  image(
+    homepage_background, 
+    homepageX, homepageY, 
+    pageWidth + pageWidth / 50, pageHeight + pageHeight / 50
+  );
+
+  // randomized if statement for flicker effect
+  if (Math.floor(random(0, 6)) == 0) {
+    image(
+      title1, 
+      50, -10, 
+      1429/3, 500/3
+    );    
+  } else {
+    image(
+      title2, 
+      50, -10, 
+      1429/3, 500/3
+    );
+    
+    scale -= 0.005;
+  }
+
+  // CAT
+  image(
+    cat, 
+    0, 90, 
+    cat.width * scale/2, cat.height * scale/2   
+  );
+
+  // start game button
+  button(start_game2, 275, 150, start_game2.width/7 * scale, start_game2.height/6 * scale);
+
+  // skins button
+  button(skins2, 410, 250, skins2.width/7 * scale, skins2.height/6 * scale);
 
 }
 
 function skinScreen() {
-  
+  scale = 1;
+  backgroundMoveSpeed = 0.5;
+
+  if (mouseX > pageWidth/2 && homepageX < 0) {
+    homepageX += backgroundMoveSpeed;
+  } else  if (mouseX < pageWidth/2 && mouseX != 0 && homepageX > -pageWidth / 50) {
+    homepageX -= backgroundMoveSpeed;
+  }
+
+  if (mouseY > pageHeight/2 && homepageY < 0) {
+    homepageY += backgroundMoveSpeed;
+  } else  if (mouseY < pageHeight/2 && mouseY != 0 && homepageY > -pageHeight / 50) {
+    homepageY -= backgroundMoveSpeed;
+  }
+
+  image(
+    homepage_background, 
+    homepageX, homepageY, 
+    pageWidth + pageWidth / 50, pageHeight + pageHeight / 50
+  );
+
+  // randomized if statement for flicker effect
+  if (Math.floor(random(0, 6)) == 0) {
+    image(
+      title1, 
+      200, 0, 
+      title1.width/4, title1.height/4
+    );    
+  } else {
+    image(
+      title2, 
+      200, 0, 
+      title2.width/4, title1.height/4
+    );
+    
+    scale -= 0.005;
+  }
+
+  // return button
+  button(return2, 20, 20, return2.width/7 * scale, return2.height/6 * scale);
 }
 
-
+function storySlides() {
+  
+}
 
 function gameover() {
 
 }
+
+function victoryPage() {
   
+}
 
 function draw() {
-  background(255);
+  background(220);
   screen(page);
 }
